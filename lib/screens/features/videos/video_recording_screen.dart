3f5016428759e43d5ef0a5e1f5006f1fd23c43e3
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-06-28 15:00:11 +0900

    06.28

--- lib/screens/features/videos/video_recording_screen.dart
+++ lib/screens/features/videos/video_recording_screen.dart
@@ -1,9 +1,12 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/screens/features/videos/widgets/video_flashbutton.dart';
+import 'package:tiktok_clone/screens/features/videos/widgets/video_preview_screen.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -13,13 +16,17 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen>
-    with TickerProviderStateMixin {
+    with TickerProviderStateMixin, WidgetsBindingObserver {
   bool _hasPermission = false;
   bool _deniedPermission = false;
   bool _isFlashModeVisible = false;
   bool _isSelfieMode = false;
 
   Icon _selectedFlashIcon = const Icon(Icons.flash_on);
+
+  double _minZoomLevel = 1.0;
+  double _maxZoomLevel = 1.0;
+  final double _currentZoomLevel = 1.0;
 
   late final AnimationController _buttonAnimationController =
       AnimationController(
@@ -57,7 +64,14 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
 
     await _cameraController.initialize();
 
+    await _cameraController.prepareForVideoRecording();
+
     _flashMode = _cameraController.value.flashMode;
+
+    _minZoomLevel = await _cameraController.getMinZoomLevel();
+    _maxZoomLevel = await _cameraController.getMaxZoomLevel();
+
+    setState(() {});
   }
 
   Future<void> initPermissions() async {
@@ -84,6 +98,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
   void initState() {
     super.initState();
     initPermissions();
+    WidgetsBinding.instance.addObserver(this);
     _progressAnimationController.addListener(() {
       setState(() {});
     });
@@ -115,14 +130,86 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
     );
   }
 
-  void _startRecording(TapDownDetails _) {
+  Future<void> _startRecording(TapDownDetails _) async {
+    if (_cameraController.value.isRecordingVideo) return;
+
+    await _cameraController.startVideoRecording();
+
     _buttonAnimationController.forward();
     _progressAnimationController.forward();
   }
 
-  void _stopRecording() {
+  Future<void> _stopRecording() async {
+    if (!_cameraController.value.isRecordingVideo) return;
+
     _buttonAnimationController.reverse();
     _progressAnimationController.reset();
+
+    final video = await _cameraController.stopVideoRecording();
+
+    if (!mounted) return;
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(
+          video: video,
+          isPicked: false,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _progressAnimationController.dispose();
+    _buttonAnimationController.dispose();
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (!_hasPermission) return;
+    if (!_cameraController.value.isInitialized) return;
+    if (state == AppLifecycleState.inactive) {
+      _cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      initCamera();
+    }
+  }
+
+  Future<void> _onPickVideoPressed() async {
+    final video = await ImagePicker().pickVideo(
+      source: ImageSource.gallery,
+    );
+    if (video == null) return;
+
+    if (!mounted) return;
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(
+          video: video,
+          isPicked: true,
+        ),
+      ),
+    );
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    double delta = details.localPosition.dy;
+
+    print(delta);
+
+    if (delta >= 0) {
+      if (_minZoomLevel > _currentZoomLevel + (-delta * 0.01)) return;
+      _cameraController.setZoomLevel(_currentZoomLevel + (-delta * 0.01));
+    } else {
+      if (_maxZoomLevel < _currentZoomLevel + (-delta * 0.01)) return;
+      _cameraController.setZoomLevel(_currentZoomLevel + (-delta * 0.01));
+    }
   }
 
   @override
@@ -182,11 +269,13 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
                                 currentFlashMode: _flashMode,
                                 onPressed: () {
                                   _setFlashMode(FlashMode.off);
-                                  setState(() {
-                                    _isFlashModeVisible = false;
-                                    _selectedFlashIcon =
-                                        const Icon(Icons.flash_off_rounded);
-                                  });
+                                  setState(
+                                    () {
+                                      _isFlashModeVisible = false;
+                                      _selectedFlashIcon =
+                                          const Icon(Icons.flash_off_rounded);
+                                    },
+                                  );
                                 },
                               ),
                             ],
@@ -196,11 +285,13 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
                                 currentFlashMode: _flashMode,
                                 onPressed: () {
                                   _setFlashMode(FlashMode.always);
-                                  setState(() {
-                                    _isFlashModeVisible = false;
-                                    _selectedFlashIcon =
-                                        const Icon(Icons.flash_on_rounded);
-                                  });
+                                  setState(
+                                    () {
+                                      _isFlashModeVisible = false;
+                                      _selectedFlashIcon =
+                                          const Icon(Icons.flash_on_rounded);
+                                    },
+                                  );
                                 },
                               ),
                               FlashModeRow(
@@ -208,11 +299,13 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
                                 currentFlashMode: _flashMode,
                                 onPressed: () {
                                   _setFlashMode(FlashMode.auto);
-                                  setState(() {
-                                    _isFlashModeVisible = false;
-                                    _selectedFlashIcon =
-                                        const Icon(Icons.flash_auto_rounded);
-                                  });
+                                  setState(
+                                    () {
+                                      _isFlashModeVisible = false;
+                                      _selectedFlashIcon =
+                                          const Icon(Icons.flash_auto_rounded);
+                                    },
+                                  );
                                 },
                               ),
                               FlashModeRow(
@@ -220,11 +313,13 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
                                 currentFlashMode: _flashMode,
                                 onPressed: () {
                                   _setFlashMode(FlashMode.torch);
-                                  setState(() {
-                                    _isFlashModeVisible = false;
-                                    _selectedFlashIcon =
-                                        const Icon(Icons.flashlight_on);
-                                  });
+                                  setState(
+                                    () {
+                                      _isFlashModeVisible = false;
+                                      _selectedFlashIcon =
+                                          const Icon(Icons.flashlight_on);
+                                    },
+                                  );
                                 },
                               ),
                             ],
@@ -234,34 +329,53 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
                     ),
                     Positioned(
                       bottom: Sizes.size40,
-                      child: GestureDetector(
-                        onTapDown: _startRecording,
-                        onTapUp: (details) => _stopRecording(),
-                        child: ScaleTransition(
-                          scale: _buttonAnimation,
-                          child: Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              SizedBox(
-                                width: Sizes.size80 + Sizes.size14,
-                                height: Sizes.size80 + Sizes.size14,
-                                child: CircularProgressIndicator(
-                                  color: Colors.red.shade400,
-                                  strokeWidth: Sizes.size6,
-                                  value: _progressAnimationController.value,
-                                ),
+                      width: MediaQuery.of(context).size.width,
+                      child: Row(
+                        children: [
+                          const Spacer(),
+                          GestureDetector(
+                            onTapDown: _startRecording,
+                            onTapUp: (details) => _stopRecording(),
+                            onPanUpdate: _onPanUpdate,
+                            child: ScaleTransition(
+                              scale: _buttonAnimation,
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  SizedBox(
+                                    width: Sizes.size80 + Sizes.size14,
+                                    height: Sizes.size80 + Sizes.size14,
+                                    child: CircularProgressIndicator(
+                                      color: Colors.red.shade400,
+                                      strokeWidth: Sizes.size6,
+                                      value: _progressAnimationController.value,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: Sizes.size80,
+                                    height: Sizes.size80,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.red.shade400,
+                                    ),
+                                  ),
+                                ],
                               ),
-                              Container(
-                                width: Sizes.size80,
-                                height: Sizes.size80,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.red.shade400,
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
-                        ),
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.center,
+                              child: IconButton(
+                                onPressed: _onPickVideoPressed,
+                                icon: const FaIcon(
+                                  FontAwesomeIcons.image,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
