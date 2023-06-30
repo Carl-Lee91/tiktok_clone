9f066e7bab213a445060f69feaa61abbe891fad7
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-06-30 13:15:50 +0900

    06.30 provider

--- lib/common/widget/main_navigation/widgets/video_configration/video_config.dart
+++ lib/common/widget/main_navigation/widgets/video_configration/video_config.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
 
-final videoConfig = ValueNotifier(false);
-final darkMode = ValueNotifier(false);
+class VideoConfig extends ChangeNotifier {
+  bool isMuted = false;
+  bool isAutoplay = false;
+  bool isDarkmode = false;
+
+  void toggleIsMuted() {
+    isMuted = !isMuted;
+    notifyListeners();
+  }
+
+  void toggleIsAutoplay() {
+    isAutoplay = !isAutoplay;
+    notifyListeners();
+  }
+
+  void toggleIsDarkmode() {
+    isDarkmode = !isDarkmode;
+    notifyListeners();
+  }
+}
